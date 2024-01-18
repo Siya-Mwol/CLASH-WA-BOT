3f8d3bd6031ae4b90b2d02cb6c235a22d20bf594
Siya-Mwol <109611007+Siya-Mwol@users.noreply.github.com>
2024-01-18 07:14:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ module.exports = {
       ? "^[.]" // forfor null just "^" add this
       : "^[.]",
   BRANCH: "master",
-  OWNER_NUMBER: ["918891871875@s.whatsapp.net", "0@s.whatsapp.net"],
-  STICKER_DATA: process.env.STICKER_DATA || "Nxiii;Nazim",
-  SUDO: process.env.SUDO || "918891871875,0",
+  OWNER_NUMBER: ["919961857267@s.whatsapp.net", "0@s.whatsapp.net"],
+  STICKER_DATA: process.env.STICKER_DATA || "CLASH-WA-BOT;TOXIC-KICHUX",
+  SUDO: process.env.SUDO || "919961857267,0",
   API: process.env.API || "https://api-viper.onrender.com", //don't change this link. If you change then some commands will not work!.
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  OWNER_NAME: process.env.OWNER_NAME || "Nazim",
-  BOT_NAME: process.env.BOT_NAME || "Nxiii",
+  OWNER_NAME: process.env.OWNER_NAME || "TOXIC-KICHU",
+  BOT_NAME: process.env.BOT_NAME || "CLASH-WA-BOT",
   WORK_TYPE: process.env.WORK_TYPE || "public",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
